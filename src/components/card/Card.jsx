509949c85aa6312c1e90deac6fbf665d8a3f145d
croy3 <c.roy3728@gmail.com>
2023-04-27 23:19:17 +0200

    add popup card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Card = ({ artistEvents, selectEvent }) => {
+const Card = ({ artistEvent, selectEvent }) => {
   const formatDate = (datetime) => {
     const date = new Date(datetime);
     const day = date.getDate();
@@ -8,39 +8,38 @@ const Card = ({ artistEvents, selectEvent }) => {
     const year = date.getFullYear();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    return `${day < 10 ? '0' + day : day}-${
-      month < 10 ? '0' + month : month
-    }-${year} / ${hours}h${String(minutes).padStart(2, '0')}`;
+    return `${day < 10 ? "0" + day : day}-${
+      month < 10 ? "0" + month : month
+    }-${year} / ${hours}h${String(minutes).padStart(2, "0")}`;
   };
 
   return (
-    <section className='card-section'>
-      {artistEvents &&
-        artistEvents.map((event) => (
-          <div
-            key={event.id}
-            className='container-card'
-            onClick={() => selectEvent(event)}
-          >
-            <div className='card'>
-              <div className='front-card'>
-                <h3 className='title-event'>{event.venue.name}</h3>
-                <p className='city-event'>{event.venue.city}</p>
-                <p className='event-date'>{formatDate(event.datetime)}</p>
-              </div>
-              <div className='back-card'>
-                <ul className='lineup'>
-                  {event.lineup.map((name, index) => {
-                    if (index > 12) {
-                      return null;
-                    }
-                    return <li key={name}>{name}</li>;
-                  })}
-                </ul>
-              </div>
+    <section className="card-section">
+      {artistEvent && (
+        <div
+          key={artistEvent.id}
+          className="container-card"
+          onClick={() => selectEvent(artistEvent)}
+        >
+          <div className="card">
+            <div className="front-card">
+              <h3 className="title-event">{artistEvent.venue.name}</h3>
+              <p className="city-event">{artistEvent.venue.city}</p>
+              <p className="event-date">{formatDate(artistEvent.datetime)}</p>
+            </div>
+            <div className="back-card">
+              <ul className="lineup">
+                {artistEvent.lineup.map((name, index) => {
+                  if (index > 12) {
+                    return null;
+                  }
+                  return <li key={name}>{name}</li>;
+                })}
+              </ul>
             </div>
           </div>
-        ))}
+        </div>
+      )}
     </section>
   );
 };
